f3d735c08b22dab7a0d4d5e0573ed1c5dbc28985
Matthew Bianchi <mattb0027@gmail.com>
2015-02-17 17:41:24 -0600

    adding more test code

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,8 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "product is in database" do
+    example = products(:wrangler)
+    product = Product.find(1)
+    assert_same example, product, "Product was not in test data"
 end
