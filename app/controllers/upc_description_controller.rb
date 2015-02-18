83da0ec5102e21d177d14f6e9189a74de8d7c48e
Matthew Bianchi <mattb0027@gmail.com>
2015-02-18 14:15:13 -0600

    Adjusted queries in contr for real data

--- app/controllers/upc_description_controller.rb
+++ app/controllers/upc_description_controller.rb
@@ -5,7 +5,7 @@ class UpcDescriptionController < ApplicationController
   	respond_with @upcs
   end
   def men
-  	@upcs = UpcDescription.where("gender = 'Men'")
+  	@upcs = UpcDescription.where("gender = 'men'")
   	respond_with @upcs
   end
 end
